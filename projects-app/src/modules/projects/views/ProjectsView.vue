86a650cbac884b79c3550eddd96b3c9ca9edbe2a
CasJuan <castrojuanig25@gmail.com>
2025-02-06 13:26:02 -0300

    [add] Funcionalidad del modal

--- projects-app/src/modules/projects/views/ProjectsView.vue
+++ projects-app/src/modules/projects/views/ProjectsView.vue
@@ -21,17 +21,35 @@
       </tbody>
     </table>
 
-    <fab-button @click="() => console.log('click')">
+    <input-modal 
+    :open="modalOpen" 
+    @close="modalOpen = false" 
+    @value="onNewValue"
+    placeholder="Ingrese el nombre del proyecto"
+    title="Nuevo proyecto"
+    subtitle="Dale un nombre unico a tu proyecto"
+    />
+
+
+    <fab-button @click="modalOpen = true">
         <AddCircle />
     </fab-button>
-
-
 
   </div>
 </template>
 
 <script setup lang="ts">
-import FabButton from '@/modules/common/components/FabButton.vue'
-import AddCircle from '@/modules/common/components/Icons/AddCircle.vue'
+import FabButton from '../../common/components/FabButton.vue'
+import AddCircle from '../../../modules/common/Icons/AddCircle.vue'
+import InputModal from '../../common/components/InputModal.vue' 
+import { ref } from 'vue';
+
+
+const modalOpen = ref(false);
+
+const onNewValue = (projectName: string) => {
+  console.log({projectName})
+}
+
 </script>
 
