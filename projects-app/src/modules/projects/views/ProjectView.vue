6a71be3a07a43b030b8b522bfd2879f9cdcd9575
CasJuan <castrojuanig25@gmail.com>
2025-02-06 21:03:59 -0300

    [add] Project View - Pantalla para ver el proyecto

---
+++ projects-app/src/modules/projects/views/ProjectView.vue
@@ -0,0 +1,33 @@
+<template>
+    <bread-crumbs :name="project?.name ?? 'Proyecto Actual'" />
+</template>
+
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import BreadCrumbs from '../../../modules/common/components/BreadCrumbs.vue'
+import { useProjectsStore } from '../store/projects.store';
+
+interface Props {
+    id: string;
+}
+
+const props = defineProps<Props>();
+const projectStore = useProjectsStore();
+const project = ref<Project|null>()
+
+
+//const project = projectStore.projectList.find( project => project.id === props.id);
+
+watch ( () => props.id, () => {
+    project.value = projectStore.projectList.find( project => project.id === props.id);
+},
+{
+    immediate: true,
+});
+
+
+
+
+
+
+</script>
